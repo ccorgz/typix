5283182fe94b34ea4087f80fa11df31afe3e58a2
Ccorgz <ccorgzdev@gmail.com>
2025-04-18 16:02:55 -0300

    Fix: Fixed index.d.ts declarations

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,40 @@
-declare module "hmacpass" {
+declare module "typix" {
+  export interface Field {
+    name: string;
+    type: string;
+    strict?: boolean;
+    validateValue?: (value: any) => boolean;
+  }
+
+  export interface Options {
+    fields: Field[];
+    strict?: boolean;
+  }
+
+  export interface ExpectedField {
+    field: string;
+    expectedType: string;
+    receivedType: string;
+    receivedValue: any;
+    errorType: "typing" | "value";
+  }
+
+  export interface ValidationResult {
+    message: string;
+    isValid: boolean;
+    expectedFields: ExpectedField[];
+  }
+
+  export function validate<T extends Record<string, any>>(
+    options: Partial<Options>,
+    data: T
+  ): Promise<ValidationResult>;
+
   export function createPass(pass: string): {
     hash: string;
     salt: string;
   };
+
   export function validatePass(
     pass: string,
     hash: string,
